90fb97ac8d9cc46c50a75275cb2e2ba4e7a02926
Brageanth David Palencia <brageanth@outlook.es>
2021-12-21 15:33:42 -0500

    feat: ssl certificate

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  console.log(process.env.PORT);
+  const httpsOptions = {
+    key: fs.readFileSync('./masterbag.com.co.key'),
+    cert: fs.readFileSync('./masterbag.com.co.crt'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
