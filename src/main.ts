ad53d025a4f467eb80ecbcd5698a4239d651df8d
Brageanth David Palencia <brageanth@outlook.es>
2021-12-22 09:03:08 -0500

    fix: cp ssl

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
+  console.log(path.resolve(__filename, './secrets/masterbag.com.co.key'));
   const httpsOptions = {
-    key: fs.readFileSync(__dirname + '/masterbag.com.co.key'),
-    cert: fs.readFileSync(__dirname + '/masterbag.com.co.crt'),
+    key: fs.readFileSync(
+      path.resolve(__dirname, './secrets/masterbag.com.co.key'),
+    ),
+    cert: fs.readFileSync(
+      path.resolve(__dirname, './secrets/masterbag.com.co.crt'),
+    ),
   };
   const app = await NestFactory.create(AppModule, { httpsOptions });
   await app.listen(process.env.PORT || 3000);
